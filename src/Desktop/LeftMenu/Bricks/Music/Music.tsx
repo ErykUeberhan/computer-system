db0e374faeca0bf4c6cfb6e01ea0f38d389c0141
ErykUeberhan <eryk.ueberhan@gmail.com>
2021-01-20 22:14:18 +0100

    Fetching recenlty used user's playlist by Spotify API

--- src/Desktop/LeftMenu/Bricks/Music/Music.tsx
+++ src/Desktop/LeftMenu/Bricks/Music/Music.tsx
@@ -15,6 +15,7 @@ function Music() {
         playlist: '',
     });
     const [code, setCode] = useState('');
+    const [deviceId, setDeviceId] = useState('');
     const token = useSelector(selectTokenSpotify);
     const dispatch = useDispatch();
 
@@ -23,21 +24,55 @@ function Music() {
     const redirectURL = 'http://localhost:3000/callback';
 
     const Login = () => {
-        const scopes = 'user-read-recently-played streaming user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state user-library-read';
+        const scopes = 'user-read-recently-played streaming user-read-email user-read-private user-read-currently-playing user-read-playback-state user-modify-playback-state user-library-read';
         window.location.href = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + clientId +
             (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURL);
     }
 
-    const _getToken = async () => {
+    const loadSpotifyPlayer = () => {
+        const player = new Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(token) },
+
+        });
+
+
+        // Error handling
+        player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        player.addListener('account_error', ({ message }) => { console.error(message); });
+        player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+        // Playback status updates
+        player.addListener('player_state_changed', state => { console.log(state); });
+
+        // Ready
+        player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+            setDeviceId(device_id);
+        });
+
+        // Not Ready
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+
+
+
+        // Connect to the player!
+        player.connect();
+    }
+
+    const _getToken = () => {
         if (code) {
-            const result = await fetch('https://accounts.spotify.com/api/token', {
+            const result = fetch('https://accounts.spotify.com/api/token', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                     'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
                 },
                 body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectURL}`,
-            }).then(res => res.json()).then((data) => { if (!token) { dispatch(setSpotifyAPI({ tokenSpotify: data.access_token })) } })
+            }).then(res => res.json()).then((data) => { dispatch(setSpotifyAPI({ tokenSpotify: data.access_token })) })
         }
     }
 
@@ -52,13 +87,14 @@ function Music() {
         }).then(res => res.json()).then(data => { setSong({ ...song, title: data.item.name, artist: data.item.artists[0].name, cover: data.item.album.images[1].url }) });
     }
 
-    const getRecentlyPlayed = () => {
-        fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=1`, {
+    const getRecentlyPlayed = async () => {
+        await fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=1`, {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + token
             }
         }).then(res => res.json()).then((data) => { console.log(data); setSong({ ...song, playlist: data.items[0].context.uri }) })
+
     }
 
     const getUserDevices = async () => {
@@ -70,9 +106,17 @@ function Music() {
         }).then(res => res.json()).then(data => console.log(data))
     }
 
+    const getUserPlaylists = async () => {
+        await fetch(`https://api.spotify.com/v1/playlists/${song.playlist.split(':')[2]}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(res => res.json()).then((data) => { console.log(data); setSong({ ...song, title: data.tracks.items[0].track.name, artist: data.tracks.items[0].track.artists[0].name, cover: data.tracks.items[0].track.album.images[1].url }) })
+    }
+
     const playbackPause = async () => {
-        const device_id = '5012088ddba2f7792fa48f74a2232ed3836149ee';
-        await fetch(`https://api.spotify.com/v1/me/player/pause`, {
+        await fetch(`https://api.spotify.com/v1/me/player/pause?device_id=${deviceId}`, {
             method: 'PUT',
             headers: {
                 'Authorization': 'Bearer ' + token
@@ -81,8 +125,7 @@ function Music() {
     }
 
     const playbackStartFirst = async () => {
-        const device_id = '5012088ddba2f7792fa48f74a2232ed3836149ee';
-        await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+        await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -90,14 +133,13 @@ function Music() {
                 'Authorization': 'Bearer ' + token
             },
             body: JSON.stringify({
-                context_uri: 'spotify:playlist:1CnMEd3MoVSEM1h1fxgaQT',
+                context_uri: song.playlist,
             })
         })
     }
 
     const playbackStart = async () => {
-        const device_id = '5012088ddba2f7792fa48f74a2232ed3836149ee';
-        await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+        await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -108,23 +150,25 @@ function Music() {
     }
 
     const playbackNext = async () => {
-        await fetch(`https://api.spotify.com/v1/me/player/next`, {
+        await fetch(`https://api.spotify.com/v1/me/player/next?device_id=${deviceId}`, {
             method: 'POST',
             headers: {
                 'Authorization': 'Bearer ' + token
             }
         })
         setTimeout(() => getNowSong(), 200);
+        setPlayPause(false);
     }
 
     const playbackBackward = async () => {
-        await fetch(`https://api.spotify.com/v1/me/player/previous`, {
+        await fetch(`https://api.spotify.com/v1/me/player/previous?device_id=${deviceId}`, {
             method: 'POST',
             headers: {
                 'Authorization': 'Bearer ' + token
             }
         })
         setTimeout(() => getNowSong(), 200);
+        setPlayPause(false);
     }
 
     useEffect(() => {
@@ -134,39 +178,11 @@ function Music() {
     }, [code]);
 
     useEffect(() => {
-        if (token) setTimeout(() => getRecentlyPlayed(), 200);
-    }, [token]);
-
-    useEffect(() => {
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            console.log('kk')
-            const player = new Spotify.Player({
-                name: 'Web Playback SDK Quick Start Player',
-                getOAuthToken: cb => { cb('BQCrPkpsggj4TArLs-KeFrjjWuJcHySmCma7FBFJ1z7sZGMNDwtOK6aySWgw3UFFVhY7GrLqy5r67Wt7wIWV0cqZIQHoK8h9jGL1j10fKoOiXH6-5VXNFoK_fj9hFh5M6oRQ4q5H5PFcpWGUWRcGhZRwd-U5ZUU0AXI'); }
-            });
-
-            // Error handling
-            player.addListener('initialization_error', ({ message }) => { console.error(message); });
-            player.addListener('authentication_error', ({ message }) => { console.error(message); });
-            player.addListener('account_error', ({ message }) => { console.error(message); });
-            player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-            // Playback status updates
-            player.addListener('player_state_changed', state => { console.log(state); });
-
-            // Ready
-            player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
-            });
-
-            // Not Ready
-            player.addListener('not_ready', ({ device_id }) => {
-                console.log('Device ID has gone offline', device_id);
-            });
-
-            // Connect to the player!
-            player.connect();
-        };
+        if (token) {
+            loadSpotifyPlayer();
+            getRecentlyPlayed();
+        }
+        //getUserPlaylists();
     }, [token])
 
     return (
@@ -174,12 +190,12 @@ function Music() {
             {
                 token ?
                     <div className='music-loggedin'>
-                        <div className='music-loggedin-image' onClick={() => { console.log(token); }}>
+                        <div className='music-loggedin-image' onClick={() => { getUserPlaylists() }}>
                             {
                                 song.cover ?
                                     <img src={song.cover} />
                                     :
-                                    null
+                                    's'
                             }
 
                         </div>
@@ -197,7 +213,7 @@ function Music() {
                             </div>
 
                             <div className='music-loggedin-info-player'>
-                                <IoIosSkipBackward className='music-loggedin-info-player-backward' onClick={() => { playbackBackward() }} />
+                                <IoIosSkipBackward className='music-loggedin-info-player-backward' onClick={() => { playbackBackward(); }} />
                                 {
                                     playPause ?
                                         playState ?
@@ -209,8 +225,7 @@ function Music() {
                                         <IoIosPause className='music-loggedin-info-player-pause' onClick={() => { playbackPause(); setPlayPause(!playPause) }} />
                                 }
 
-
-                                <IoIosSkipForward className='music-loggedin-info-player-forward' onClick={() => { playbackNext(); getNowSong() }} />
+                                <IoIosSkipForward className='music-loggedin-info-player-forward' onClick={() => { playbackNext(); }} />
                             </div>
                             <div className='music-loggedin-info-artist' onClick={getUserDevices}>
                                 <p className='music-loggedin-info-artist-text'>{song.artist}</p>
@@ -220,7 +235,7 @@ function Music() {
                     </div>
                     :
                     <div className='music-loggedout'>
-                        <div className='music-loggedout-icon' onClick={() => { getNowSong(); console.log(token) }}>
+                        <div className='music-loggedout-icon' onClick={() => { console.log(token) }}>
                             <ImSpotify />
                         </div>
                         <div className='music-loggedout-button' onClick={() => { Login() }}>
