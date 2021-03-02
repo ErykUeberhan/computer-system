5d229eec5d085d1322bf89e93068ea75a988afb4
ErykUeberhan <eryk.ueberhan@gmail.com>
2021-03-02 23:58:25 +0100

    Working at spotify window

--- src/Desktop/Spotify/Spotify.tsx
+++ src/Desktop/Spotify/Spotify.tsx
@@ -4,7 +4,15 @@ import './Spotify.sass'
 function Spotify() {
     return (
         <div className='spotify'>
-            SZPOTIFY
+            <div className='spotify-body'>
+                <div className='spotify-body-playlists'>
+                    <p>PLAYLISTS</p>
+                </div>
+                <div className='spotify-body-songs'>
+
+                </div>
+            </div>
+            <div className='spotify-player'></div>
         </div>
     )
 }
